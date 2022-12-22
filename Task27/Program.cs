30afe247f1eb501e1fae3b38c4994f1297fb31f7
anna.v.glazunova@yandex.ru <AnnaGlazunova>
2022-12-22 16:56:08 +0300

    Task 27 done

---
+++ Task27/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Enter a number:");
+int num = int.Parse(Console.ReadLine());
+int sum=0;
+while (num%10>0)
+{
+   sum=sum+num%10;
+   num=num/10;
+}
+Console.WriteLine("Sum=" + sum);
